682b37e157446617511d7a30bb199f7a48261821
Will Carhart <wcarhart@sandiego.edu>
2018-11-11 12:20:04 -0800

    Added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+yes | cp -rf ~/Konphig/.bash_aliases ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.bash_variables ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.bash_functions ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.bash_profile ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.bashrc ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.git-prompt.sh ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.gitconfig ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.profile ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.tmux.conf ~ >/dev/null 2>&1
+yes | cp -rf ~/Konphig/.vimrc ~ >/dev/null 2>&1
