f1a286b7c3997dc5453bb5a013becb45a09bbbbe
Will Carhart <wcarhart@sandiego.edu>
2019-05-30 22:30:12 -0700

    Individualized more functions

---
+++ bash_functions/linux/kp.sh
@@ -0,0 +1,19 @@
+# kill process by name
+kp() {
+	if [[ $# -ne 1 ]] ; then
+		echo "kp: err: incorrect number of arguments"
+		return 1
+	fi
+
+	ps aux | grep "$1" > /dev/null
+	PID=`pgrep "$1"`
+	if [[ "$PID" != "" ]] ; then
+		kill -9 $PID
+		if [[ "$?" == "0" ]] ; then
+			echo "PID $PID ($1) killed"
+		fi
+	else
+		echo "kp: err: no processes killed, could not find PID for process $1"
+		return 1
+	fi
+}
