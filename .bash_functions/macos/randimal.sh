37cf95543d1c9d4c482d15b52f4efddfc31d59f1
Will Carhart <wcarhart@sandiego.edu>
2019-06-03 13:01:19 -0700

    Individualized more functions

---
+++ .bash_functions/macos/randimal.sh
@@ -0,0 +1,8 @@
+# random animal speech
+randimal() {
+	if [[ $# -eq 0 ]] ; then
+		fortune | cowsay -f `cowsay -l | tail -n +2 | tr ' ' '\n' | shuf -n 1` | lolcat
+	else
+		echo "$1" | cowsay -f `cowsay -l | tail -n +2 | tr ' ' '\n' | shuf -n 1` | lolcat
+	fi
+}
