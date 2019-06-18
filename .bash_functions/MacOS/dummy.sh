c4aa2c15ce35a841d5f0b1ede047ed6fc5e2a587
Will Carhart <wcarhart@sandiego.edu>
2019-06-17 23:06:58 -0700

    Accidentally deleted a bunch of work, Macintosh'd more functions :(

---
+++ .bash_functions/MacOS/dummy.sh
@@ -0,0 +1,4 @@
+# function that does nothing (great for testing purposes)
+dummy() {
+	return 0
+}
---
+++ .bash_functions/MacOS/dup.sh
@@ -0,0 +1,13 @@
+# get disk usage in a nicer format
+dup() {
+	DIR=${1:-"./"}
+	DIR=`echo "$DIR/" | sed -e 's;//;/;'`
+	if [[ ! -d "$DIR" ]] ; then
+		echo "dup: err: $1 is not a valid directory"
+	fi
+
+	du -sh --apparent-size "$DIR"*
+	DU=`gdu -sh --apparent-size "$DIR"`
+	read -ra ARR <<<"$DU"
+	echo " TOTAL: ${ARR[0]}"
+}
