f0f58e91c5ad61fd7cf122e9e07a597210d19f2d
Will Carhart <wcarhart@sandiego.edu>
2019-06-18 00:08:45 -0700

    Macintosh'd more functions

---
+++ .bash_functions/MacOS/show.sh
@@ -0,0 +1,16 @@
+# list all files of specific type
+show() {
+	if [[ $# -eq 0 ]] ; then
+		echo "show: err: incorrect number of arguments"
+		return 1
+	elif [[ $# -eq 1 ]] ; then
+		find . -type f -name "*.$1"
+	else
+		while [[ $# -ne 0 ]] ; do
+			printf "\033[94m.$1 files:\033[0m\n"
+			find . -type f -name "*.$1"
+			echo
+			shift
+		done
+	fi
+}
