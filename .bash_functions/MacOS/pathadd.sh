fdbac36121988bc5e044627dfffa25daccd89704
Will Carhart <wcarhart@sandiego.edu>
2019-06-17 23:56:17 -0700

    Macintosh'd more functions

---
+++ .bash_functions/MacOS/pathadd.sh
@@ -0,0 +1,16 @@
+# add to PATH variable
+pathadd() {
+	if [[ $# -eq 0 ]] ; then
+		echo "pathadd: err: incorrect number of arguments"
+		return 1
+	fi
+
+	NEW=${1%/}
+	if [[ -d "$1" ]] && ! echo "$PATH" | grep -E -q "(^|:)$NEW($|:)" ; then
+		if [[ "$2" == "after" ]] ; then
+			export PATH="$PATH:$NEW"
+		else
+			export PATH="$NEW:$PATH"
+		fi
+	fi
+}
